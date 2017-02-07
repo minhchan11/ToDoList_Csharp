dbf8e2cd56aa83ee29d1007f883f4d06e08cff1b
Alexandra Holcombe and Minh Phuong <alexandra+minh@>
2017-02-07 08:49:40 -0800

    Add private static List, public static list and void for that instance

--- Objects/Task.cs
+++ Objects/Task.cs
@@ -3,6 +3,7 @@ namespace ToDoList.Objects
   public class Task
   {
     private string _description;
+    private static List<string> _instances = new List<string> {};
     public Task (string description)
     {
       _description = description;
@@ -14,6 +15,14 @@ namespace ToDoList.Objects
     public void SetDescription(string newDescription)
     {
       _description = newDescription;
+    }
+    public static List<string> GetAll()
+    {
+      return _instances;
+    }
+    public void Save()
+    {
+      _instances.Add(_description);
     }
   }
 }
