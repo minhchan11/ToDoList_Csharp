9c2c4595e550ecd5adab5ef70b3cca0b674dafcc
Minh Phuong and Renee Mei <minh+renee@>
2017-02-08 09:21:42 -0800

    add on a new string private int ID, make public in object Task, call function count on ID, nest function Add in object task, call public find function

--- Objects/Task.cs
+++ Objects/Task.cs
@@ -4,13 +4,19 @@ namespace ToDoList.Objects
 {
   public class Task
   {
+    // Declare private variables that we will use
     private string _description;
-    private static List<string> _instances = new List<string>{};
-
+    private int _id;
+    // Make a list of all instances
+    private static List<Task> _instances = new List<Task>{};
+    // Create a public object from the original
     public Task (string description)
     {
       _description = description;
+      _instances.Add(this);
+      _id = _instances.Count;
     }
+    //Set,Get only for private variables
     public string GetDescription()
     {
       return _description;
@@ -19,17 +25,19 @@ namespace ToDoList.Objects
     {
       _description = newDescription;
     }
-    public static List<string> GetAll()
+    public static List<Task> GetAll()
     {
       return _instances;
     }
-    public void Save()
-    {
-      _instances.Add(_description);
-    }
+    //Methods to clear all instances in List
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+    //Method to search and find instances in List
+    public static Task Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 }
